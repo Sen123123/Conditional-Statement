e3b90e4d00704c2e6b7b11924b33f67e597e7965
Sen123123 <usen.summerfield@gmail.com>
2025-01-23 22:05:23 +0600

    first commit

---
+++ Conditional Statement.py
@@ -0,0 +1,16 @@
+# number = int(input("Enter an integer: "))
+# if number %2==0:
+#     print("It's an even number", number)
+# else:
+#     print("It's an odd number", number)
+
+Value = int(input("Enter a value ---> "))
+
+if Value>50:
+    if Value %2==0:
+     print ("This number is bigger than 50 and an even number, :", Value)
+    else:
+     print ("This number is bigger than 50 and an odd number, :", Value)
+
+else:
+    print ("This number is less than 50, :", Value)    
